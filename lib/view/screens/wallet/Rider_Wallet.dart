978be91af6b5fe25df8562b3106d95cc5b222d69
Mujtaba Ali <mujtaba@experts-global.com>
2024-03-27 10:52:20 +0500

    Cash Collection Merged and Completed

--- lib/view/screens/wallet/Rider_Wallet.dart
+++ lib/view/screens/wallet/Rider_Wallet.dart
@@ -3,7 +3,10 @@ import 'package:efood_multivendor_driver/controller/rider_wallet_controller.dart
 import 'package:efood_multivendor_driver/view/screens/wallet/widget/cash_in_hand.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../controller/laundry_notification_controller.dart';
+import '../../../helper/route_helper.dart';
 import '../../../util/dimensions.dart';
+import '../../../util/images.dart';
 
 
 class RiderWallet extends StatefulWidget {
@@ -30,83 +33,68 @@ class _RiderWalletState extends State<RiderWallet> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).cardColor,
-      //   // leading: Image.asset(Images.logo,width: 10,height: 10,),
-      //   title: Image.asset(
-      //     Images.logo_name,
-      //     width: 150,
-      //     height: 150,
-      //   ),
-      //    actions: [
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).cardColor,
+        // leading: Image.asset(Images.logo,width: 10,height: 10,),
+        title: Image.asset(
+          Images.logo_name,
+          width: 150,
+          height: 150,
+        ),
+         actions: [
     
-      //     InkWell(
-      //       onTap: () {
-      //         Get.find<LaundryNotificationController>().clearNotification();
-      //         Get.find<LaundryNotificationController>().getNotificationList(1);
-      //         Get.toNamed(RouteHelper.getLaundryNotificationRoute());
-      //       },
+          InkWell(
+            onTap: () {
+              Get.find<LaundryNotificationController>().clearNotification();
+              Get.find<LaundryNotificationController>().getNotificationList(1);
+              Get.toNamed(RouteHelper.getLaundryNotificationRoute());
+            },
     
-      //       child: GetBuilder<LaundryNotificationController>(
+            child: GetBuilder<LaundryNotificationController>(
     
-      //           builder: (notificationController) {
-      //         bool _hasNewNotification = false;
-      //         if (notificationController
-      //                 .paginationLaundryNotificationModel?.data !=
-      //             null) {
-      //           _hasNewNotification = notificationController
-      //                   .paginationLaundryNotificationModel?.data?.length !=
-      //               notificationController.getSeenNotificationCount();
-      //         }
-      //         return Container(
-      //           alignment: Alignment.center,
-      //           height: 44,
-      //           width: 44,
-      //           child: Stack(
-      //             children: [
-      //               Icon(
-      //                 Icons.notifications,
-      //                  size: 24,
-      //                 color: Theme.of(context).textTheme.bodyLarge.color,
-      //                 ),
+                builder: (notificationController) {
+              bool _hasNewNotification = false;
+              if (notificationController
+                      .paginationLaundryNotificationModel?.data !=
+                  null) {
+                _hasNewNotification = notificationController
+                        .paginationLaundryNotificationModel?.data?.length !=
+                    notificationController.getSeenNotificationCount();
+              }
+              return Container(
+                alignment: Alignment.center,
+                height: 44,
+                width: 44,
+                child: Stack(
+                  children: [
+                    Icon(
+                      Icons.notifications,
+                       size: 24,
+                      color: Theme.of(context).textTheme.bodyLarge.color,
+                      ),
                  
-      //               _hasNewNotification
-      //                   ? Positioned(
-      //                       top: 2,
-      //                       right: 2,
-      //                       child: Container(
-      //                         height: 8,
-      //                         width: 8,
-      //                         decoration: BoxDecoration(
-      //                             shape: BoxShape.circle,
-      //                             color: Theme.of(context).primaryColor),
-      //                       ),
-      //                     )
-      //                   : SizedBox(),
-      //             ],
-      //           ),
-      //         );
-      //       }),
-      //     ),
+                    _hasNewNotification
+                        ? Positioned(
+                            top: 2,
+                            right: 2,
+                            child: Container(
+                              height: 8,
+                              width: 8,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Theme.of(context).primaryColor),
+                            ),
+                          )
+                        : SizedBox(),
+                  ],
+                ),
+              );
+            }),
+          ),
     
-          // ToggleButton(title: "test",isButtonActive: isButtonActive,
-          //     onTap: (){
-          //       setState(() {
-          //     isButtonActive = !isButtonActive;
-          //   });
-          //     },
-          //     ),
-          //  GetBuilder<RiderWalletController>(
-          //   builder: (riderwalletcontroller) {
-          //    return ToggleButton(title: "test",isButtonActive: riderwalletcontroller.isButtonActive,
-          //     onTap: (){
-          //       riderwalletcontroller.setButtonActive(!riderwalletcontroller.isButtonActive);
-          //     },
-          //     );
-          //   } ,
-          //  )
-      //    ],
-      // ),
+        
+         ],
+      ),
       body:  
       Scrollbar(child: SingleChildScrollView(
         padding: EdgeInsets.all(Dimensions.PADDING_SIZE_SMALL),
